287fb953f98c3fed9c0c8edac9692ff8256a2a3c
vicentesilvadev <vicentesilva.dev@gmail.com>
2026-06-19 09:33:58 -0300

    feat(Espaco): Criação da List de recursos e a criação base dos metodos

--- Sistema_de_Reserva/Espaco.java
+++ Sistema_de_Reserva/Espaco.java
@@ -1,17 +1,30 @@
 package Sistema_de_Reserva;
 
+import java.util.*;
+
 public class Espaco {
     
     private int idEspaco;
     private String nome;
     private int capacidadeMax;
     private double precoBase;
+    private List<String> recursos;
+    private boolean disponibilidade = true;
 
     public Espaco(int id, String nome, int capacidadeMax, double precoBase){
         this.idEspaco = id;
         this.nome = nome;
         this.capacidadeMax = capacidadeMax;
         this.precoBase = precoBase;
+        this.recursos = new ArrayList<>();
+    }
+
+    public boolean VerificarDisponibilidade(){
+        return true;
+    }
+
+    public double PrecoFinal(){
+       return 0.0; 
     }
 
 }
