a2d1dac6f688659c73185b9e100349b3ef99624c
vicentesilvadev <vicentesilva.dev@gmail.com>
2026-06-23 14:50:29 -0300

    feat(Usuario): criei a classe Usuario, fiz a base dos atributos e dos metodos

---
+++ Sistema_de_Reserva/Usuario.java
@@ -0,0 +1,25 @@
+package Sistema_de_Reserva;
+
+public class Usuario {
+    
+    private int idUsuario;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Usuario(int id, String nome, String email, String telefone){
+        this.idUsuario = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public void RealizarReseva(){
+
+    }
+
+    public void ValidarDocumento(){
+        
+    }
+
+}
