050a35ab70438e8a92626877564b870243f76cb7
vicentesilvadev <vicentesilva.dev@gmail.com>
2026-07-01 21:49:29 -0300

    feat(ClienteJuridico):criei metodo get e validardocumento

--- Sistema_de_Reserva/ClienteJuridico.java
+++ Sistema_de_Reserva/ClienteJuridico.java
@@ -1,19 +1,25 @@
 package Sistema_de_Reserva;
 
 public class ClienteJuridico extends Usuario {
-    
-     private String CNPJ;
+
+    private String CNPJ;
 
     public ClienteJuridico(int id, String nome, String email, String telefone, String CNPJ){
         super(id, nome, email, telefone);
         this.CNPJ = CNPJ;
     }
 
-    @Override
-    public boolean ValidarDocumento(){
-        return true;
+    public String getCNPJ(){
+        return CNPJ;
     }
 
-    
-}
+    @Override
+    public boolean ValidarDocumento(){
+        if (CNPJ == null) {
+            return false;
+        }
+        String apenasNumeros = CNPJ.replaceAll("[^0-9]", "");
+        return apenasNumeros.length() == 14;
+    }
 
+}
